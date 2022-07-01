b9e75e98df029a96e20ce0691a885cc3da28ed32
Rama Hemphill <ramah808@gmail.com>
2022-06-30 19:00:00 -0700

    did more stuff

--- javascript.js
+++ javascript.js
@@ -1,6 +1,10 @@
 let generator = Math.random()
 let computerHand;
 let humanHand;
+let submission;
+let humanSubmission = "rock"
+let submitButton;
+let rounds = 0;
 
 function computerPlay(){
     rnd = Math.random()
@@ -16,35 +20,47 @@ function computerPlay(){
     }
 }
 
-function humanPlay(){
-    rnd = Math.random()
-    stringOfRnd = rnd.toString()
-    fourthDigit = stringOfRnd.charAt(4)
 
-    if(fourthDigit <= 3){
-        return "paper"
-    } else if (fourthDigit >=7) {
-        return "rock"
-    } else {
-        return "scissors"
-    }
+function humanPlay(){
+   return humanSubmission
 }
 
-function winnerDeclaration () {
-    computerHand = computerPlay();
-    humanHand = humanPlay();
+function winnerDeclaration(a,b) {
+    let computerHand = computerPlay();
+    let humanHand = humanPlay();
 
     if (humanHand == computerHand){
+        rounds +=1 
         return "It's a fucking tie"
     } else if ((humanHand == "rock" && computerHand == "scissors") 
             || (humanHand == "paper" && computerHand == "rock") 
             || (humanHand == "scissors" && computerHand == "paper")) {
+                rounds +=1 
                 return "Human Victory!!!"
-    } else {
+    } else if ((computerHand == "rock" && humanHand == "scissors") 
+            || (computerHand == "paper" && humanHand == "rock") 
+            || (computerHand == "scissors" && humanHand == "paper"))
+        {
+        rounds +=1 
         return "The computer has beaten the human contestant!"
-    }
+    } else { return "no contest"}
 }
 
+function gameResults(){
+    for (let i=0; i < 5; i++){
 
-
-console.log(winnerDeclaration())
+        if (winnerDeclaration() == "It's a fucking tie"){
+            rounds +=1
+            console.log(rounds)
+        } else if (winnerDeclaration() == "Human Victory!!!"){
+            rounds +=1
+            console.log(rounds)
+        } else if (winnerDeclaration() == "Human Victory!!!"){
+            rounds +=1
+            console.log(rounds)
+    } else {
+            rounds +=1
+            console.log(rounds)
+    }
+}
+}
