d4415449d98a4872118e38063b230ac6ff777400
flipflopverb <ramah808@gmail.com>
2023-10-14 17:49:38 -0700

    added buttons for the ui

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-let generator = Math.random()
+let generator = Math.random();
 let computerHand;
 let humanHand;
 let humanSubmission;
@@ -10,95 +10,123 @@ let computerScore = 0;
 let humanGameScore = 0;
 let computerGameScore = 0;
 
-function computerPlay(){
-    rnd = Math.random()
-    stringOfRnd = rnd.toString()
-    fourthDigit = stringOfRnd.charAt(4)
-    if(fourthDigit <= 3){
-        document.getElementById("computerChoice").innerHTML = "rock"
-        computerHand = "rock"
-    } else if (fourthDigit >= 8) {
-        document.getElementById("computerChoice").innerHTML = "paper"
-        computerHand = "paper"
+function computerPlay() {
+  rnd = Math.random();
+  stringOfRnd = rnd.toString();
+  fourthDigit = stringOfRnd.charAt(4);
+  if (fourthDigit <= 3) {
+    document.getElementById('computerChoice').innerHTML = 'Rock';
+    computerHand = 'Rock';
+  } else if (fourthDigit >= 8) {
+    document.getElementById('computerChoice').innerHTML = 'Paper';
+    computerHand = 'Paper';
+  } else {
+    document.getElementById('computerChoice').innerHTML = 'Scissors';
+    computerHand = 'Scissors';
+  }
+  console.log(computerHand);
+}
+
+function humanPlay(e) {
+  const x = e.target.value;
+  humanHand = x;
+  document.getElementById('humanChoice').innerHTML = humanHand;
+  computerPlay();
+  //alert('human ' + humanHand + ' vs computer:' + computerHand);
+  roundDeclaration();
+
+  if (round % 10 == 0) {
+    if (humanScore > computerScore) {
+      humanGameScore++;
+      computerScore = 0;
+      humanScore = 0;
     } else {
-        document.getElementById("computerChoice").innerHTML = "scissors"
-        computerHand = "scissors"
+      computerGameScore++;
+      computerScore = 0;
+      humanScore = 0;
     }
-    console.log(computerHand)
+
+    document.getElementById('humanGameScoreCount').innerHTML = humanGameScore;
+    document.getElementById('computerGameScoreCount').innerHTML =
+      computerGameScore;
+    document.getElementById('computerScoreCount').innerHTML = computerScore;
+    document.getElementById('humanScoreCount').innerHTML = humanScore;
+
+    //document.getElementById('submitButton').disable = true;
+  } else {
+    //document.getElementById('submitButton').disable = false;
+  }
 }
 
-
-
-function humanPlay(){
-    humanHand = document.getElementById("humanInput").value.toLowerCase()
-    document.getElementById("humanChoice").innerHTML = humanHand
-    console.log(humanHand+" human")
-}
+// old functionality
+//function humanPlay(){
+//humanHand = document.getElementById("humanInput").value.toLowerCase()
+//document.getElementById("humanChoice").innerHTML = humanHand
+//console.log(humanHand+" human")
+//}
 
 function roundDeclaration() {
-    round ++
-    document.getElementById("roundCount").innerHTML = round
-    if (humanHand == computerHand){
-        document.getElementById("roundResult").innerHTML = "tie for "
-    }else if ((humanHand == "rock") && (computerHand == "paper")){
-        document.getElementById("roundResult").innerHTML = "human loses"     
-        computerScore ++
-    }else if ((humanHand == "rock") && (computerHand == "scissors")){
-        document.getElementById("roundResult").innerHTML = "human wins"    
-        humanScore ++        
-    }else if ((humanHand == "paper") && (computerHand == "rock")){
-        document.getElementById("roundResult").innerHTML = "human wins"     
-        humanScore ++
-    }else if ((humanHand == "paper") && (computerHand = "scissors")){
-        document.getElementById("roundResult").innerHTML = "human loses"  
-        computerScore ++
-    }else if ((humanHand == "scissors") && (computerHand == "paper")){
-        document.getElementById("roundResult").innerHTML = "human wins"   
-        humanScore ++
-    }else if ((humanHand == "scissors") && (computerHand == "rock")){
-        document.getElementById("roundResult").innerHTML = "human loses"    
-        computerScore ++
-    }else {
-        document.getElementById("roundResult").innerHTML = "due to incompetent spelling the human loses"
-        computerScore ++
-    }
+  round++;
+  document.getElementById('roundCount').innerHTML = round;
+  if (humanHand == computerHand) {
+    document.getElementById('roundResult').innerHTML = 'tie for ';
+  } else if (humanHand == 'Rock' && computerHand == 'Paper') {
+    document.getElementById('roundResult').innerHTML = 'human loses';
+    computerScore++;
+  } else if (humanHand == 'Rock' && computerHand == 'Scissors') {
+    document.getElementById('roundResult').innerHTML = 'human wins';
+    humanScore++;
+  } else if (humanHand == 'Paper' && computerHand == 'Rock') {
+    document.getElementById('roundResult').innerHTML = 'human wins';
+    humanScore++;
+  } else if (humanHand == 'Paper' && (computerHand = 'Scissors')) {
+    document.getElementById('roundResult').innerHTML = 'human loses';
+    computerScore++;
+  } else if (humanHand == 'Scissors' && computerHand == 'Paper') {
+    document.getElementById('roundResult').innerHTML = 'human wins';
+    humanScore++;
+  } else if (humanHand == 'Scissors' && computerHand == 'Rock') {
+    document.getElementById('roundResult').innerHTML = 'human loses';
+    computerScore++;
+  } else {
+    document.getElementById('roundResult').innerHTML =
+      'due to incompetent spelling the human loses';
+    computerScore++;
+  }
 
-    document.getElementById("computerScoreCount").innerHTML = computerScore
-    document.getElementById("humanScoreCount").innerHTML = humanScore
+  document.getElementById('computerScoreCount').innerHTML = computerScore;
+  document.getElementById('humanScoreCount').innerHTML = humanScore;
 }
 
-function gameDeclaration(){
-    humanPlay()
-    computerPlay()
-    roundDeclaration()
-
-    if((round%10)==0){
-
-        if(humanScore>computerScore){
-            humanGameScore ++
-            computerScore = 0
-            humanScore = 0
-        } else {
-            computerGameScore ++
-            computerScore = 0
-            humanScore = 0
-        }
-
-        document.getElementById("humanGameScoreCount").innerHTML = humanGameScore
-        document.getElementById("computerGameScoreCount").innerHTML = computerGameScore
-        document.getElementById("computerScoreCount").innerHTML = computerScore
-        document.getElementById("humanScoreCount").innerHTML = humanScore
-
-        document.getElementById("submitButton").disable = true;
-
-
+function gameDeclaration() {
+  if (round % 10 == 0) {
+    if (humanScore > computerScore) {
+      humanGameScore++;
+      computerScore = 0;
+      humanScore = 0;
     } else {
-        document.getElementById("submitButton").disable = false;
+      computerGameScore++;
+      computerScore = 0;
+      humanScore = 0;
     }
 
+    document.getElementById('humanGameScoreCount').innerHTML = humanGameScore;
+    document.getElementById('computerGameScoreCount').innerHTML =
+      computerGameScore;
+    document.getElementById('computerScoreCount').innerHTML = computerScore;
+    document.getElementById('humanScoreCount').innerHTML = humanScore;
 
-
+    document.getElementById('submitButton').disable = true;
+  } else {
+    document.getElementById('submitButton').disable = false;
+  }
 }
 
+const rockButton = document.querySelector('#rockButton');
+rockButton.addEventListener('click', humanPlay);
 
+const paperButton = document.querySelector('#paperButton');
+paperButton.addEventListener('click', humanPlay);
 
+const scissorsButton = document.querySelector('#scissorsButton');
+scissorsButton.addEventListener('click', humanPlay);
